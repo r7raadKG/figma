f0718eb55f6544aab79301204a91375fb3fdca90
r7raadKG <kyrill.gerashchenko@gmail.com>
2025-11-24 15:48:57 +0300

    Create Metro Bonus landing page layout

---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,29 @@
+import 'vuetify/styles';
+import '@mdi/font/css/materialdesignicons.css';
+import { createVuetify } from 'vuetify';
+import { aliases, mdi } from 'vuetify/iconsets/mdi';
+
+const vuetify = createVuetify({
+  theme: {
+    defaultTheme: 'dark',
+    themes: {
+      dark: {
+        dark: true,
+        colors: {
+          background: '#0C1E3A',
+          surface: '#10284C',
+          primary: '#F5C400',
+          secondary: '#00A7E1',
+          accent: '#F25E2B',
+        },
+      },
+    },
+  },
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: { mdi },
+  },
+});
+
+export default vuetify;
